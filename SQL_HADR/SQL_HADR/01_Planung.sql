821fdb5c72d6b3818ab89fffa5f39ee8eeb1a4ae
Rauchendraser <andreasr@ppedv.de>
2023-04-27 12:56:10 +0200

    Logshipping

---
+++ SQL_HADR/SQL_HADR/01_Planung.sql
@@ -0,0 +1,29 @@
+/*
+
+Ausgangssituation HV-SQL1
+
+SQL Agent  schulung\sqlAgent  
+SQL Server schulung\svcSQL
+
+Kennwort ppedv2019!
+
+
+Pfade
+DB Daten C:\_SQLDB\
+DB Logs: C:\_SQLDB\
+Backup:  c:\_backup
+
+MAXDOP: max Anzahl der Kerne für eine Abfrage , die mehr als per default 5 SQL Dollar benötigt
+
+
+RAM MAX 2700
+
+IP: Hv-SQ1L 192.168.137.2
+		    192.168.138.2
+
+IP: Hv-SQL2 192.168.137.3
+		    192.168.138.3
+
+
+
+*/
